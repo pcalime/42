b55ab4754a187c1d9ee1fcbd1c3bba33eb257308
Paul CALIME <pcalime@e3r3p12.42.fr>
2015-12-16 16:36:52 +0100

    update

--- Fillit/ft_resolve.c
+++ Fillit/ft_resolve.c
@@ -5,30 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcalime <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/15 16:24:08 by pcalime           #+#    #+#             */
-/*   Updated: 2015/12/15 19:24:13 by pcalime          ###   ########.fr       */
+/*   Created: 2015/12/16 16:16:41 by pcalime           #+#    #+#             */
+/*   Updated: 2015/12/16 16:34:53 by pcalime          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_resolve(t_tetrimini *tab_piece, char ***tab_final, int nb, int end) //end ==ft_counttetri
+int		ft_resolve(t_tetrimini *tab_piece, char ***tab_final, int nb, int end)
 {
-	int cmpt;
+	int	cmpt;
 
-	if (nb == end) (on a fini le tableau est rempli)
+	cmpt = 0;
+	if (nb == end)
 		return (1);
 	while (cmpt < end)
 	{
-		fonction qui regarde si la piece(tab_piece[cmpt]) et deja placer (cherche la lettre)	
-			si oui -->cmpt++;(on essaye de placer une autre piece)
-			si non -->put_piece;(rentre la piece dans le tableau)
-				-->si put_piece return(0)
-					cmpt++;
-				-->si put_piece return(1) (alors if resolve nb++)
-				if (ft_resolve(nb++))(passe a la recursive suivante)
-					return (1);(si toute les recursive return(1) alors c GG)
-			ft_retire la piece;(recupe la lettre(tab_piece[cmpt].letter) et la remplace par des '.')
+		if (ft_check_piece(tab_piece[cmpt], tab_final0[0]) == 0)	//a faire
+			cmpt++;
+		else
+		{
+			if (ft_put_piece(tab_piece[cmpt], &tab_final[0]) == 0) //a faire
+				cmpt++;
+			else
+			{
+				if (ft_resolve(tab_piece, &tab_final[0], nb + 1, end))
+					return (1);
+				ft_retire_piece(&tab_final[0], tab_piece[cmpt]); //a faire
+			}
+		}
 	}
-	return (0); (la fonction revient a la recursive d avant et reprend la ou elle etait)
+	return (0);
 }
