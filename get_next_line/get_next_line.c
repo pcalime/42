b3443d264f8a7a76dbb542ddaa0b16d74579eafc
Paul CALIME <pcalime@e2r6p5.42.fr>
2016-02-12 15:37:52 +0100

    push

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pcalime <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 17:14:56 by pcalime           #+#    #+#             */
-/*   Updated: 2016/02/09 17:21:41 by pcalime          ###   ########.fr       */
+/*   Updated: 2016/02/12 15:37:40 by pcalime          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,19 @@
 
 int		get_next_line(int const fd, char **line)
 {
+	static	gnl_t	gnl;
+	int		ret;
 	
+	if (fd < 0)
+		return (-1);
+	gnl.buf = malloc(sizeof(char) * BUFF_SIZE);
+	gnl.ptr = malloc(sizeof(char) * BUFF_SIZE);
+	while (ret = read(fd, gnl.buf, BUFF_SIZE))
+	{
+		if (ret == -1)
+			return (ret);
+		if (ret == 0)
+			return (ret);
+		
+	}
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pcalime <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/08 16:53:14 by pcalime           #+#    #+#             */
-/*   Updated: 2016/02/09 17:21:38 by pcalime          ###   ########.fr       */
+/*   Updated: 2016/02/09 17:24:48 by pcalime          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@
 
 typedef static struct	gnl_s
 {
-	int fd;
 	char *buf;
 	char *ptr;
 }						gnl_t;
